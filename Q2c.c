cde15dd3a3292a61205fe19895954145cc52c1a0
Floriant Poclet <floriant.poclet@gmail.com>
2025-12-11 10:43:00 +0100

    Ajout du fichier Q2c.c (retour au prompt)

--- Q2c.c
+++ Q2c.c
@@ -1,3 +1,46 @@
-//
-// Created by flori on 11/12/2025.
-//
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+#define WELCOME_MSG "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n"
+#define PROMPT "enseash % "
+#define BUFFER_SIZE 256
+
+int main(void) {
+    char buffer[BUFFER_SIZE];
+
+    write(STDOUT_FILENO, WELCOME_MSG, strlen(WELCOME_MSG));
+
+    while (1) {
+        write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
+
+        ssize_t size = read(STDIN_FILENO, buffer, BUFFER_SIZE - 1);
+
+
+        buffer[size] = '\0';
+
+        char *newline = strchr(buffer, '\n');
+        if (newline)
+            *newline = '\0';
+
+
+        if (strcmp(buffer, "exit") == 0)
+            break;
+
+
+
+        pid_t pid = fork();
+
+        if (pid == 0) {
+
+            execlp(buffer, buffer, NULL);
+
+
+            _exit(1);
+        } else {
+            wait(NULL);
+        }
+    }
+
+    return 0;
+}
